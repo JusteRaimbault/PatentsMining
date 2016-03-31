4bd408ccd8b2d55af38434782d87c4b3944cef8a
JusteRaimbault <raimbaultjwin@gmail.com>
2016-03-31 20:43:14 +0200

    dat_collection_ok

--- Models/DataCollection/dataCollection.py
+++ Models/DataCollection/dataCollection.py
@@ -3,5 +3,18 @@
 # Collect and store data
 #   - retrieving and unzipping done in shell ; here file name as arg -
 
-import pymongo
+import pymongo,sys
 import parser
+
+def import_dat_file(f):
+    print 'importing file '+str(f)
+    mongo = pymongo.MongoClient('localhost', 29019)
+    database = mongo['redbook']
+    database.raw.create_index('id')
+
+    year = f.split('.')[0]
+    data = parse_dat_file(f,year)
+
+    database.raw.insert_many(data)
+
+import_dat_file(sys.argv[0])
