34030d1e41ab061de2de86fa3b500a9b7648e2a7
JusteRaimbault <raimbaultjwin@gmail.com>
2016-05-11 10:47:43 +0200

    cit_nw_construction

---
+++ Models/Citation/constructNW.R
@@ -0,0 +1,19 @@
+
+# construct citation network
+
+setwd(paste0(Sys.getenv('CS_HOME'),'/PatentsMining/Data/processed/citation'))
+
+library(igraph)
+library(Matrix)
+
+edf1 = read.csv(file=paste0(Sys.getenv('CS_HOME'),'/PatentsMining/Data/raw/citation/citation75_99/citation75_99.csv'),stringsAsFactors = FALSE)
+edf2 = read.csv(file=paste0(Sys.getenv('CS_HOME'),'/PatentsMining/Data/raw/citation/citation00_10.csv'),stringsAsFactors = FALSE)
+
+edf = rbind(edf1[,c(1,6)],edf2[,c(1,6)])
+names(edf)<-c("from","to")
+
+gcitation = graph_from_data_frame(edf)
+citadjacency = get.adjacency(gcitation,sparse=TRUE)
+
+save(gcitation,file='network/citationNetwork.RData')
+save(citadjacency,file='network/adjacency.RData')
