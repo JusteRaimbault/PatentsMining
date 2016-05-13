91887676dc2ca2f5a0b14130faa6ed697eb63ea4
Juste Raimbault <juste.raimbault@polytechnique.edu>
2016-05-13 15:38:15 +0200

    sparse_techno_matrices_probas

--- Models/Techno/prepareData.R
+++ Models/Techno/prepareData.R
@@ -11,6 +11,10 @@ sizeTh = 10
 
 for(year in years){
   show(year)
-  d = Matrix(read.table(file=paste0('technoProbas_',year,'_sizeTh',sizeTh),sep=";",row.names = TRUE,header=TRUE),sparse=TRUE)
-  save(d,file=paste0(Sys.getenv('CS_HOME'),'/PatentsMining/Data/processed/classes/technoPerYear/technoProbas_',year,'_sizeTh',sizeTh,'.RData'))
+  d = read.table(file=paste0('technoProbas_',year,'_sizeTh',sizeTh),sep=";",row.names = 1,header=TRUE)
+  #d[is.na(d)]=0
+  m = Matrix(as.matrix(d),sparse=TRUE)
+  save(m,file=paste0(Sys.getenv('CS_HOME'),'/PatentsMining/Data/processed/classes/technoPerYear/technoProbas_',year,'_sizeTh',sizeTh,'.RData'))
 }
+
+
