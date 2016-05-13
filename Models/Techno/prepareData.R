043751cee0166bfeac6bb31608bd44828e2e5cc9
JusteRaimbault <raimbaultjwin@gmail.com>
2016-05-13 14:54:54 +0200

    sparse_mats

--- Models/Techno/prepareData.R
+++ Models/Techno/prepareData.R
@@ -3,12 +3,14 @@
 
 setwd(paste0(Sys.getenv('CS_HOME'),'/PatentsMining/Models/Techno/TechnoClasses/res/technoPerYear'))
 
+library(Matrix)
+
 years = 1976:2012
 
 sizeTh = 10
 
 for(year in years){
   show(year)
-  d = read.table(file=paste0('technoProbas_',year,'_sizeTh',sizeTh),sep=";",row.names = TRUE,header=TRUE)
+  d = Matrix(read.table(file=paste0('technoProbas_',year,'_sizeTh',sizeTh),sep=";",row.names = TRUE,header=TRUE),sparse=TRUE)
   save(d,file=paste0(Sys.getenv('CS_HOME'),'/PatentsMining/Data/processed/classes/technoPerYear/technoProbas_',year,'_sizeTh',sizeTh,'.RData'))
 }
