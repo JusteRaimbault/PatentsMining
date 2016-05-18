13a7fd9ae41a4c4c8c9b7269b7b8a38a949b0292
JusteRaimbault <raimbaultjwin@gmail.com>
2016-05-18 11:12:05 +0200

    uncompressed_techno_matrices

--- Models/Semantic/semanalysis.R
+++ Models/Semantic/semanalysis.R
@@ -18,12 +18,18 @@ loadProbas<-function(year){
   load(paste0('probas/relevant_',year,semprefix))
   res$semprobas=probas[,3:ncol(probas)]
   load(paste0(technoprefix,year,'_sizeTh',sizeTh,'.RData'))
+  rownames(m)<-sapply(rownames(m),function(s){ifelse(nchar(s)==8,substring(s,2),s)})
   rowstoadd=setdiff(rownames(res$semprobas),rownames(m))
   m=rbind(m,matrix(0,length(rowstoadd),ncol(m)));rownames(m)[(nrow(m)-length(rowstoadd)+1):nrow(m)]=rowstoadd
   res$technoprobas = m[rownames(res$semprobas),]
   return(res)
 }
 
+
+# for(year in years){
+#   load(paste0(technoprefix,year,'_sizeTh',sizeTh,'.RData'))
+#   save(m,file=paste0(technoprefix,year,'_sizeTh',sizeTh,'_uncompressed.RData'),compress=FALSE)
+# }
 
 ##
 #  1) First order interdisciplinarity
@@ -34,16 +40,17 @@ overlaps = c();cyears=c()
 for(year in years){
   res = loadProbas(year);technoprobas=res$technoprobas;semprobas=res$semprobas
   inds = which(colSums(technoprobas)>50)
-  currentovs = rep(0,ncol(technoprobas)*(ncol(technoprobas)-1)/2)
+  #currentovs = rep(0,ncol(technoprobas)*(ncol(technoprobas)-1)/2)
+  currentovs = rep(0,ncol(semprobas)*(ncol(semprobas)-1)/2)
   k=1
-  for(i in 1:(length(inds)-1)){show(i);for(j in (i+1):length(inds)){
-    currentovs[k]=sum(technoprobas[,inds[i]]*technoprobas[,inds[j]])/nrow(technoprobas);k=k+1
+  #for(i in 1:(length(inds)-1)){show(i);for(j in (i+1):length(inds)){
+  #  currentovs[k]=sum(technoprobas[,inds[i]]*technoprobas[,inds[j]])/nrow(technoprobas);k=k+1
+  #}}
+  for(i in 1:(ncol(semprobas)-1)){show(i);for(j in (i+1):ncol(semprobas)){
+    currentovs=append(currentovs,sum(semprobas[,i]*semprobas[,j])/nrow(semprobas));
   }}
-  # for(i in 1:(ncol(semprobas)-1)){show(i);for(j in (i+1):ncol(semprobas)){
-  #   overlaps=append(overlaps,sum(semprobas[,i]*semprobas[,j])/nrow(semprobas));
-  # }}
   overlaps=append(overlaps,currentovs)
-  cyears=append(cyears,rep(year,ncol(technoprobas)*(ncol(technoprobas)-1)/2))
+  cyears=append(cyears,rep(year,length(currentovs)))
 }
 
 #save(overlaps,cyears,file='res/techno_overlaps.RData')
@@ -51,7 +58,7 @@ load(file='res/techno_overlaps.RData')
 
 inds=overlaps>0#1:length(overlaps)#
 #overlaps[overlaps==0]=1e-10
-g=ggplot(data.frame(overlap=overlaps[inds],year=cyears[inds]),aes(x=year,y=overlap))
+g=ggplot(data.frame(overlap=overlaps[inds],year=cyears[inds]),aes(x=overlap,colour=as.character(year)))#aes(x=year,y=overlap))
 g+geom_density(alpha=0.25)+scale_x_log10()+xlab("overlap")+ylab("density")#+scale_y_log10()
 g+geom_point(pch='.')+scale_y_log10()+stat_smooth()
 
@@ -66,9 +73,10 @@ for(year in years){
   cyears=append(cyears,rep(year,nrow(semprobas)+nrow(technoprobas)))
 }
 
-df = data.frame(originality=origs[types=="techno"],year=as.character(cyears[types=="techno"]),type=types[types=="semantic"])
+df = data.frame(originality=origs[types=="techno"],year=as.character(cyears[types=="techno"]),type=types[types=="techno"])
 g=ggplot(df)
 g+geom_density(aes(x=originality,colour=year))
+
 
 ##
 #  2) Layers macro-structure comparison
@@ -100,29 +108,30 @@ g+geom_density(aes(x=overlaps,colour=cyears),alpha=0.25,adjust=0.75)+scale_x_log
 #  3) Second order interdisciplinarity (citation)
 
 # load citation matrix
-load(paste0(Sys.getenv('CS_HOME'),'/PatentsMining/Data/processed/citation/adjacency.RData'))
+load(paste0(Sys.getenv('CS_HOME'),'/PatentsMining/Data/processed/citation/network/adjacency.RData'))
 
 # 3.1) Patent level
 
-origs=c();years=c();types=c();
+origs=c();cyears=c();types=c();
 for(year in years){
-  res = loadProbas(year);technoprobas=res$technoprobas;semprobas=res$semprobas
-  currentadj = adjacency[rownames(technoprobas),rownames(technoprobas)]
+  res = loadProbas(year);technoprobas=Matrix(as.matrix(res$technoprobas));semprobas=Matrix(as.matrix(res$semprobas))
+  currentnames=intersect(rownames(technoprobas),rownames(citadjacency))
+  currentadj = citadjacency[currentnames,currentnames]
   currentadj = diag(rowSums(currentadj))%*%currentadj
-  technocit = currentadj%*%technoprobas
-  semcit = currentadj%*%semprobas
+  technocit = currentadj%*%technoprobas[currentnames,]
+  semcit = currentadj%*%semprobas[currentnames,]
   origs = append(origs,1 - rowSums(technocit^2));types=append(types,rep("techno",nrow(technocit)))
   origs = append(origs,1 - rowSums(semcit^2));types=append(types,rep("semantic",nrow(semcit)))
-  years=append(years,rep(year,nrow(technocit)+nrow(semcit)))
+  cyears=append(cyears,rep(year,nrow(technocit)+nrow(semcit)))
 }
 
-g=ggplot(data.frame(originality=origs,year=years,type=types))
-g+geom_density(aes(x=origs,colour=year,linetype=type))
+g=ggplot(data.frame(originality=origs[types=="semantic"],year=as.character(cyears[types=="semantic"])))
+g+geom_density(aes(x=originality,colour=year))
 
 
 # 3.2) class level
 
-origs=c();years=c();types=c();
+origs=c();cyears=c();types=c();
 for(year in years){
   res = loadProbas(year);technoprobas=res$technoprobas;semprobas=res$semprobas
   currentadj = adjacency[rownames(technoprobas),rownames(technoprobas)]
@@ -131,7 +140,7 @@ for(year in years){
   semcit = t(semprobas)%*%(currentadj%*%semprobas)
   origs = append(origs,1 - rowSums(technocit^2));types=append(types,rep("techno",nrow(technocit)))
   origs = append(origs,1 - rowSums(semcit^2));types=append(types,rep("semantic",nrow(semcit)))
-  years=append(years,rep(year,nrow(technocit)+nrow(semcit)))
+  cyears=append(cyears,rep(year,nrow(technocit)+nrow(semcit)))
 }
 
 inds=1:length(origs)#overlaps>0
