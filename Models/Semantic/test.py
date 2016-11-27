b38b1a724857215fdbf26232bb0b49c4ec3a1988
Juste Raimbault <juste.raimbault@polytechnique.edu>
2016-11-27 14:24:00 +0100

    full_sens

--- Models/Semantic/test.py
+++ Models/Semantic/test.py
@@ -19,10 +19,11 @@ def npatent_years():
     data = []
     window=utils.get_parameter('window-size')
     for year in range(1980,2013):
-        years=range((year-window+1),(year+1))
+	print(year)
+        years=map(lambda y:str(y),range(int(year-window+1),int(year+1)))
         patents = mongo['patent']['keywords'].find({"app_year":{"$in":years}},no_cursor_timeout=True)
         npatents = patents.count()
-        yearrange = years[0]+"-"+years[len(years)-1]
+        yearrange = str(years[0])+"-"+str(years[len(years)-1])
         data.append([yearrange,npatents])
     utils.export_csv(data,'data/patentcount_window'+str(window)+'.csv',";","yearrange;count")
 
