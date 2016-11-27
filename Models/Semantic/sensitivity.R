de495662b0f34f86916fc3df53aec890460141bd
JusteRaimbault <raimbaultjwin@gmail.com>
2016-11-27 20:25:19 +0800

    patent_count

--- Models/Semantic/sensitivity.R
+++ Models/Semantic/sensitivity.R
@@ -13,11 +13,20 @@ kwLimit="100000.0"
 eth="10.0"
 
 
-
-year=2004
+for(year in wyears){
+year=1994
 yearrange = paste0((year-windowSize+1),"-",year)
-
-
 sensdata = as.tbl(read.csv(file=paste0('sensitivity/sensitivity_',yearrange,"_",kwLimit,"_eth",eth,".csv"),sep=";",header=TRUE))
+argmaxs=sensdata%>% group_by(dispth)%>%summarise(argmaxeth=eth[comnum==max(comnum)][1])
+mean(argmaxs$argmaxeth)
 
+g=ggplot(sensdata[sensdata$dispth>0.06,] %>% group_by(dispth,eth) %>% summarise(maxmod=max(modularity),comnum=min(comnum),vcount=mean(vcount)))
+g+geom_line(aes(x=eth,y=maxmod,colour=dispth,group=dispth))
+g+geom_point(aes(x=maxmod,y=vcount,colour=dispth))
+g+geom_point(aes(x=vcount,y=maxmod,colour=dispth))
+g+geom_point(aes(x=vcount,y=comnum,colour=dispth))
+
+# TODO : for each year, plot triobjective, highlight chosen points
+
+}
 
--- Models/Semantic/test.py
+++ Models/Semantic/test.py
@@ -3,11 +3,27 @@
 
 import pymongo,pickle
 from igraph import *
-import graph
+import graph,utils
 
 
-years = ['1976','1977','1978','1979','1980']
-kwLimit=100000.0
-min_edge_th=10.0
+def test_construct_graph() :
+    years = ['1976','1977','1978','1979','1980']
+    kwLimit=100000.0
+    min_edge_th=10.0
 
-graph.construct_graph(years,kwLimit,min_edge_th)
+    graph.construct_graph(years,kwLimit,min_edge_th)
+
+
+def npatent_years():
+    mongo = pymongo.MongoClient(utils.get_parameter('mongopath',True,True))
+    data = []
+    for year in range(1980,2013):
+        years=range()
+        patents = mongo['patent']['keywords'].find({"app_year":{"$in":years}},no_cursor_timeout=True)
+        npatents = patents.count()
+        yearrange = years[0]+"-"+years[len(years)-1]
+        data.append([yearrange,npatents])
+    window=utils.get_parameter('window-size')
+    utils.export_csv(data,'data/patentcount_window'+str(window)+'.csv',";","yearrange;count")
+
+npatent_years()
