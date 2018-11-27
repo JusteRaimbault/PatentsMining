5f81a842e602173dd8c7a37e337f240b732b7391
Juste Raimbault <juste.raimbault@polytechnique.edu>
2018-11-27 11:57:45 +0100

    .

---
+++ Models/Database/simpleexport.sh
@@ -0,0 +1,16 @@
+
+# auth params
+USER=`cat user`
+PWD=`cat psswd`
+AUTHDB=`cat authdb`
+PORT=`cat port`
+
+DB='redbook'
+COLLECTION='raw'
+
+QUERYFILE=$1
+#FIELDS='{"abstract":1,"title":1,"id":1,"year":1,"grant_date":1}'
+FIELDS="abstract,title,id,year"
+
+mongoexport -u $USER -p $PWD --authenticationDatabase $AUTHDB --port $PORT --db $DB --collection $COLLECTION -f $FIELDS --queryFile $QUERYFILE --limit 2 
+
