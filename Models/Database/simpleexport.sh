6e308ede947cf0c63803e7a0221232d59afc9a0a
Juste Raimbault <juste.raimbault@polytechnique.edu>
2020-03-25 16:42:44 +0100

    database export

--- Models/Database/simpleexport.sh
+++ Models/Database/simpleexport.sh
@@ -8,9 +8,10 @@ PORT=`cat port`
 DB='redbook'
 COLLECTION='raw'
 
-QUERYFILE=$1
+#QUERYFILE=$1
 #FIELDS='{"abstract":1,"title":1,"id":1,"year":1,"grant_date":1}'
-FIELDS="abstract,title,id,year"
+#FIELDS="abstract,title,id,year"
 
-mongoexport -u $USER -p $PWD --authenticationDatabase $AUTHDB --port $PORT --db $DB --collection $COLLECTION -f $FIELDS --queryFile $QUERYFILE --limit 2 
+#mongoexport -u $USER -p $PWD --authenticationDatabase $AUTHDB --port $PORT --db $DB --collection $COLLECTION -f $FIELDS --queryFile $QUERYFILE --limit 2 
+mongoexport -u $USER -p $PWD --authenticationDatabase $AUTHDB --port $PORT --db $DB --collection $COLLECTION -o res/redbook.json
 
